b127a8d72d38d7803598fa1b75de1694a48aacd8
Kidkiwi <josh.ethan.sagar@gmail.com>
2020-10-05 17:35:59 +1300

    Initial Upload

---
+++ Assets/Sample App/Scripts/MyLog.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyLog : MonoBehaviour
+{
+    string myLog;
+    List<string> myLogQueue = new List<string>();
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        myLog = logString;
+
+        string newString = "\n [" + type + "] : " + myLog;
+
+        myLogQueue.Insert(0, (newString));
+
+        if (type == LogType.Exception)
+        {
+            newString = "\n" + stackTrace;
+            myLogQueue.Insert(0, (newString));
+        }
+
+        myLog = string.Empty;
+
+        foreach (string mylog in myLogQueue)
+        {
+            myLog += mylog;
+        }
+    }
+
+    private void Update()
+    {
+        GetComponent<TMPro.TextMeshProUGUI>().text = myLog;
+    }
+}
---
+++ Assets/Sample App/Scripts/SubscriptionHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Purchasing;
+using DG.Tweening;
+
+public class SubscriptionHandler : MonoBehaviour
+{
+    #region Variables
+    const string _monthlySubID = "m_subscription";
+    const string _yearlySubID = "y_subscription";
+
+    [SerializeField] TextMeshProUGUI _txtMessage = null;
+    [SerializeField] GameObject _pnSub = null;
+    [SerializeField] GameObject _pnContent = null;
+
+    [Header("IAP Buttons")]
+    [SerializeField] IAPButton _btnMonthlySub = null;
+    [SerializeField] IAPButton _btnYearlySub = null;
+    #endregion
+
+    #region Functions
+    private void Awake()
+    {
+        _btnMonthlySub.onPurchaseComplete.AddListener((v) => { PurchaseSuccess(v); });
+        _btnYearlySub.onPurchaseComplete.AddListener((v) => { PurchaseSuccess(v); });
+
+        _btnMonthlySub.onPurchaseFailed.AddListener((v, v2) => { PurchaseFailed(v, v2); });
+        _btnYearlySub.onPurchaseFailed.AddListener((v, v2) => { PurchaseFailed(v, v2); });
+
+        _txtMessage.text = "";
+    }
+
+    public void PurchaseSuccess(Product product)
+    {
+        switch(product.definition.id)
+        {
+            case _monthlySubID:
+                _txtMessage.text = "Monthly sub purchased!";
+                break;
+            case _yearlySubID:
+                _txtMessage.text = "Yearly sub purchased!";
+                break;
+        }
+
+        _txtMessage.color = Color.green;
+
+
+        // Hide popup.
+        _pnSub.GetComponent<RectTransform>().DOAnchorPosX(-1000f, .75f)
+            .SetEase(Ease.Linear)
+            .OnComplete(() => { _pnSub.SetActive(false); });
+
+        _pnContent.SetActive(true);
+    }
+
+    public void PurchaseFailed(Product product, PurchaseFailureReason purchaseFailureReason)
+    {
+        Debug.Log("Purchase failed: " + product.definition.id);
+
+        switch (product.definition.id)
+        {
+            case _monthlySubID:
+                _txtMessage.text = "Monthly sub failed!\n" + purchaseFailureReason;
+                break;
+            case _yearlySubID:
+                _txtMessage.text = "Yearly sub failed!\n" + purchaseFailureReason;
+                break;
+        }
+
+        _txtMessage.color = Color.red;
+
+        // Just in case content is active.
+        _pnSub.SetActive(true);
+        _pnContent.SetActive(false);
+    }
+    #endregion
+}
